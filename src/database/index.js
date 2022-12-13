dcc1addc331d6a75b1b7d0090ddc629875ca031f
Eldar Amirgazin <tmshulk@gmail.com>
2022-12-13 11:03:15 +0500

    complete v-1.0.3

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", true);
 mongoose.connect(
     process.env.MONGO_URL,
     (err) => {
