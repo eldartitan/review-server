94df2507bf5343fca7d1d2ba36314c11f415d2c0
Eldar Amirgazin <tmshulk@gmail.com>
2022-12-13 10:49:35 +0500

    complete v-1.0.2

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const MONGO_URL = process.env.MONGO_URL;
 
+mongoose.set("strictQuery", false);
 mongoose.connect(
-    MONGO_URL,
+    process.env.MONGO_URL,
     (err) => {
         if (err) console.log(err);
         else console.log("mongdb is connected");
