411fda888370af347704b6ca4192fc0f2598003c
Eldar Amirgazin <tmshulk@gmail.com>
2023-01-05 10:06:49 +0500

    2.1 commit

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
